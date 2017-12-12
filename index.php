17a420031d0a8ce0b1225c40ca4518e066e25cf6
daivaj <34283132+daivaj@users.noreply.github.com>
2017-12-12 22:04:20 +0200

    Failas php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php1uzduotis</title>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="reset.css">
+    <link rel="stylesheet" type="text/css" href="style.css?v=3">
+</head>
+
+<body>
+<h1>Lentelė</h1>
+<table class="table" border="1">
+    <tr>
+        <th class="th" colspan="2">Pavadinimas</th>
+        <th>Veiksmas</th>
+    </tr>
+    <tr>
+        <td class="td" colspan="3"> Vaisiai</td>
+    </tr>
+    <tr>
+        <td class="td1">Obuolys</td>
+        <td><img src="http://www.asvyras.lt/turinys/galerija/obuolys.jpg"></td>
+        <td class="td1">
+            <button>Pirkti</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="td1">Apelsinas</td>
+        <td><img src="http://www.segroup.lt/images/uploader/or/180x180.c/orange_istock_000001331357x_540x405-1.jpg">
+        </td>
+        <td class="td1">
+            <button>Parduoti</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="td" colspan="3">Daržovės</td>
+    </tr>
+    <tr>
+        <td class="td1">Morka</td>
+        <td><img src="http://time.valgo.me/wp-content/uploads/2016/09/kiek-virti-morka.jpg"></td>
+        <td class="td1">
+            <button>Pirkti</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="td1">Kopūstas</td>
+        <td><img src="http://www.sulieknek.lt/wp-content/uploads/2013/05/kopustas_baltaguzis_cabagge_sveika-mityba.jpg">
+        </td>
+        <td class="td1">
+            <button>Parduoti</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="td" colspan="3">Pieno produktai</td>
+    </tr>
+    <tr>
+        <td class="td1">Pienas</td>
+        <td><img src="http://www.dykai.eu/fun/content/2010/10/romitelos-pienas/1259559116_rrrrryer2.jpg"></td>
+        <td class="td1">
+            <button>Pirkti</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="td1">Sviestas</td>
+        <td><img src="http://www.manosveikata.lt/resources/_catalog/dvaras21.jpg"></td>
+        <td class="td1">
+            <button>Parduoti</button>
+        </td>
+    </tr>
+</table>
+
+</body>
+</html>
